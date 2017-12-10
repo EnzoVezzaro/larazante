48ebffa9b9c947153bf99e93a9c78b8e44ae153c
EnzoVezzaro <enzo-vezzaro@live.it>
2017-12-10 19:33:27 +0100

    carousel

--- static/js/custom.js
+++ static/js/custom.js
@@ -9,3 +9,54 @@ function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
     document.getElementById("main").style.marginLeft = "0";
 }
+
+/* Slick Carousel */
+$('.carousel').slick({
+  centerMode: true,
+  centerPadding: '0px',
+  slidesToShow: 3,
+  refresh: true,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 2
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
+});
+
+$( document ).ready(function(){
+  $('#carousel_crew').css('display', 'none');
+});
+
+$( '#carousel_cast_link' ).click(function(){
+  $('#carousel_cast').css('display', 'flex');
+  $('#carousel_crew').css('display', 'none');
+});
+
+$( '#carousel_crew_link' ).click(function(){
+  $('#carousel_cast').css('display', 'none');
+  $('#carousel_crew').css('display', 'block');
+});
